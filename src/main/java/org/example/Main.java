d464c68df20238e6b7602f41cb428aeb45052d8a
Matheus Henrique Costa Oliveira <matheushenrll@outlook.com>
2025-12-04 18:37:53 -0300

    ultimo commit 04/12/2025

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,9 @@
 package org.example;
 
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +16,41 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
+
+
+        Conexao cx = new Conexao();
+        Connection conn = null;
+
+
+        conn = cx.getConnection();
+        if( conn == null)
+            System.out.println("A conexão não ocorreu");
+        else
+            System.out.println("O banco de dados está conectado");
+
+
+        System.out.println(" ");
+
+        System.out.println("Nome do Cliente: ");
+        String nome= sc.nextLine();
+        System.out.println("Fone do Cliente: ");
+        String fone = sc.nextLine();
+
+        String sql = "INSERT INTO Usuario (nome,telefone) values ('"+nome+"','"+fone+"')";
+        //System.out.println(sql);
+
+        ResultSet rs = null;
+        try {
+            Statement st = conn.createStatement();
+
+            int intRs = st.executeUpdate(sql);
+            if (intRs > 0)
+                System.out.println("Inclusão relaizada com sucesso");
+            else
+                System.out.println("Inclusão não realizada");
+        } catch (SQLException e){
+            System.out.println("erro ao inserir no banco: "+ e.getMessage());
+        }
 
     }
 }
