ba0e038eece820c3e1164e5b72a6dea7d6766d5c
matheuscoliv <matheushenrll@outlook.com>
2025-12-06 16:30:08 -0300

    validação dos dados de cadastro

--- frontend/apptechfood.js
+++ frontend/apptechfood.js
@@ -5,15 +5,21 @@ function showPassword () {
     const eye=document.getElementById('eye');
     const eyeSlash=document.getElementById('eye-slash');
     const fieldPassword=document.getElementById('field-password');
+    const fieldConfirmacao=document.getElementById('field-confirmacao');
+    const ver = document.getElementById('ver');
+    const naoVer = document.getElementById('naoVer');
 
-    if(eye.style.display ==='none') {
+
+    if(eye.style.display ==='none' || ver.style.display ==='none') {
         eye.style.display = 'block';
         eyeSlash.style.display = 'none';
         fieldPassword.type = 'text';
+        fieldConfirmacao.type = 'text';
     } else {
         eye.style.display = 'none';
         eyeSlash.style.display = 'block';
         fieldPassword.type = 'password';
+        fieldConfirmacao.type = 'password';
 
     }
 }
@@ -26,3 +32,22 @@ document.getElementById('btn-login').addEventListener('click', function(e) {
         window.location.href = "index.html";
     }
 })
+document.getElementById('btn-cadastrar').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert("Redirecionando para pagina de cadastro")
+    window.location.href = "cadastro.html";
+})
+
+function verificarCadastro() {
+    const fieldPassword = document.getElementById('field-password');
+    const fieldConfirmacao = document.getElementById('field-confirmacao');
+    const btnCadastrar = document.getElementById('btn-cadastrarpage');
+
+    if(fieldPassword.value.length < 4 || fieldConfirmacao.value.length < 4){
+        alert("Senha invalida, tente mais caracteres");
+    }
+
+    if(fieldPassword != fieldConfirmacao) {
+        alert('A confirmação falhou')
+    }
+}
