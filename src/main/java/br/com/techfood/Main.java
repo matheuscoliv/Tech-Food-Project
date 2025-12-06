0c05ae88c7deb5a118fe4e6cc41e5809d0a7b4ff
matheuscoliv <matheushenrll@outlook.com>
2025-12-06 12:14:47 -0300

    refatoração de arquivos, pastas e diretorios

--- src/main/java/br/com/techfood/Main.java
+++ src/main/java/br/com/techfood/Main.java
@@ -2,19 +2,12 @@ package br.com.techfood;
 
 import br.com.techfood.usuarios.UsuarioIdoso;
 
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String comorbidade = "";
-
 
         UsuarioIdoso meuIdoso = new UsuarioIdoso("Conceição", "123456", 72, "Diabetica");
         meuIdoso.criarPlano();
         meuIdoso.criarLembrete();
-
 
     }
 }
